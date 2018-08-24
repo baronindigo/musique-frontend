e7695053c7cb115a883912d690a8b6518ad1bab1
Carlos Chavez <carlos.chavez@hotmail.com>
2018-08-23 22:53:12 -0700

    Initial Files.

---
+++ src/Firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey            : "",
+    authDomain        : "",
+    databaseURL       : "",
+    projectId         : "",
+    storageBucket     : "",
+    messagingSenderId : ""
+}
+
+export default firebase.initializeApp(config);
