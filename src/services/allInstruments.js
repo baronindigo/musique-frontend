6e852fd981af738b08ecc4348f614785a651339e
Levi Vazquez Romo <hlvr.13@gmail.com>
2018-08-28 19:21:28 -0700

    getToken() usado

--- src/services/allInstruments.js
+++ src/services/allInstruments.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 import getToken from '../resolvers/getToken';
 import constant from '../const';
 
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFAZ21haWwuY29tIiwiaWQiOiI1Yjg0ZWI1ZDZkMWZjMTNlZWM3YzY5MWYiLCJpYXQiOjE1MzU0Mzc4MDUsImV4cCI6MTUzNTUyNDIwNX0.k_ONNtzMkKxDnTrf8XYjkjT3rQIP8JulDvw9gJKdass'
 
 export default () => {
     return axios({
@@ -17,6 +16,6 @@ export default () => {
                     }
                 }
             `
-        }, headers: {'Authorization':'JWT ' + token}
+        }, headers: {'Authorization':'JWT ' + getToken()}
     })
 }
--- src/services/allUsers.js
+++ src/services/allUsers.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 import getToken from '../resolvers/getToken';
 import constant from '../const';
 
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFAZ21haWwuY29tIiwiaWQiOiI1Yjg0ZWI1ZDZkMWZjMTNlZWM3YzY5MWYiLCJpYXQiOjE1MzU0Mzc4MDUsImV4cCI6MTUzNTUyNDIwNX0.k_ONNtzMkKxDnTrf8XYjkjT3rQIP8JulDvw9gJKdass'
-
 export default () => {
     return axios({
         url: constant.url + 'graphql',
@@ -23,6 +21,6 @@ export default () => {
                     }
                 }
             `
-        }, headers: {'Authorization':'JWT ' + token}
+        }, headers: {'Authorization':'JWT ' + getToken()}
     })
 }
