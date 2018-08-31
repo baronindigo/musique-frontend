6443a0230569d2507ab742fc78ab32603dc6fbc2
Rafael <rafael_x@hotmail.com>
2018-08-30 21:16:25 -0700

    heroku

--- src/const.js
+++ src/const.js
@@ -1,4 +1,4 @@
 export default {
-    url   : "http://localhost:3000/",
-    local : "http://localhost:3000/",
+    url   : "https://musicsoul.herokuapp.com/",
+    local : "https://musicsoul.herokuapp.com/",
 }
